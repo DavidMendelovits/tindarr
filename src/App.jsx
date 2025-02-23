e0a715f217cc8b27fc103ffdee00a6aa58861c35
david Mendelovits <david.mendelovits@gmail.com>
2025-02-22 17:57:43 -0800

    sets app based on url

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import PirateDatingApp from './components/PirateDatingApp'
 
 function App() {
   return (
-    <PirateDatingApp type="grindr" />
+    <PirateDatingApp type={window.location.pathname.includes('grindarr') ? 'grindr' : 'tinder'} />
   )
 }
 
